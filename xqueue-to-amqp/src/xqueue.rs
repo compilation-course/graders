3f4cccc3341058cc40aef82625cc8534be334e8d
Samuel Tardieu <sam@rfc1149.net>
2018-02-10 06:54:27 +0100

    Add xqueue.rs file

---
+++ xqueue-to-amqp/src/xqueue.rs
@@ -0,0 +1,20 @@
+// Example from edX.org
+// {
+//   "xqueue_header": {
+//     "submission_id": 12,
+//     "submission_key": "280587728458c29e1e66ae0c54a806f4"
+//   }
+//   "xqueue_files": {
+//     "helloworld.c": "http://download.location.com/helloworld.c"
+//   }
+//   "xqueue_body":
+//   "{
+//     "student_info": {
+//       "anonymous_student_id": "106ecd878f4148a5cabb6bbb0979b730",
+//       "submission_time": "20160324104521",
+//       "random_seed": 334
+//     },
+//     "student_response": "def double(x):\n return 2*x\n",
+//     "grader_payload": "problem_2"
+//    }"
+// }
