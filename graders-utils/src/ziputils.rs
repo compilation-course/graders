b04f5a643538d8b5e49e74da470f11b6da2c7e09
Samuel Tardieu <sam@rfc1149.net>
2018-02-20 19:32:41 +0100

    Add zip_recursive()

--- graders-utils/src/ziputils.rs
+++ graders-utils/src/ziputils.rs
@@ -1,10 +1,10 @@
 use reqwest::{self, StatusCode};
 use std::fs::{self, File};
-use std::io::{Error, ErrorKind, Result};
-use std::io::prelude::*;
+use std::io::{self, Error, ErrorKind, Result};
 use std::os::unix::fs::PermissionsExt;
 use std::path::{Path, PathBuf};
 use zip;
+use zip::write::{FileOptions, ZipWriter};
 
 /// Unzip `zip_file` in `dir`, ensure that all paths start with `dragon_tiger/`,
 /// and return the path to the `dragon_tiger` directory.
@@ -35,9 +35,7 @@ pub fn unzip(dir: &PathBuf, zip_file: &str) -> Result<PathBuf> {
         } else {
             trace!("creating file {:?}", target_file);
             let mut target = File::create(target_file)?;
-            let mut content = Vec::with_capacity(file.size() as usize);
-            file.read_to_end(&mut content)?;
-            target.write_all(&content)?;
+            io::copy(&mut file, &mut target)?;
             let mut perms = target.metadata()?.permissions();
             perms.set_mode(file.unix_mode().unwrap_or(0o600));
             target.set_permissions(perms)?;
@@ -64,4 +62,36 @@ fn unzip_url(dir: &PathBuf, url: &str) -> Result<PathBuf> {
     zip.copy_to(&mut target)
         .map_err(|e| Error::new(ErrorKind::Other, format!("cannot write zip file: {}", e)))?;
     unzip(dir, target_file.to_str().unwrap())
+}
+
+/// Recursively build zip while setting the top level name
+pub fn zip_recursive(dir: &Path, top_level: &str, zip_file: &Path) -> Result<()> {
+    trace!("in zip_recursive({:?}, {}, {:?}", dir, top_level, zip_file);
+    let writer = File::create(zip_file)?;
+    let mut zip = ZipWriter::new(writer);
+    add_to_zip(&mut zip, &dir.to_owned(), &Path::new(top_level).to_owned())
+}
+
+fn add_to_zip(zip_file: &mut ZipWriter<File>, dir: &PathBuf, dir_in_zip: &PathBuf) -> Result<()> {
+    trace!("in add_to_zip(FILE, {:?}, {:?})", dir, dir_in_zip);
+    zip_file.add_directory(format!("{}/", dir_in_zip.to_string_lossy()), FileOptions::default())?;
+    for entry in fs::read_dir(dir)? {
+        let path = entry?.path();
+        let full_path = dir.join(&path);
+        let metadata = full_path.metadata()?;
+        let file_name = path.file_name().unwrap().to_string_lossy();
+        let zip_path = dir_in_zip.join(&*file_name);
+        trace!("considering {:?} as {:?}", full_path, zip_path);
+        if metadata.is_dir() {
+            add_to_zip(zip_file, &full_path, &zip_path)?;
+        } else if metadata.is_file() {
+            let mode = metadata.permissions().mode();
+            zip_file.start_file(
+                zip_path.to_string_lossy(),
+                FileOptions::default().unix_permissions(mode),
+            )?;
+            io::copy(&mut File::open(&full_path)?, zip_file)?;
+        }
+    }
+    Ok(())
 }
