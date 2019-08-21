d4f28b1d8f43df31f04ec56f66b1d898bca06082
Pablo Oliveira <pablo@sifflez.org>
2019-08-21 11:43:20 +0200

    Comment code and improve students' html report

--- xqueue-to-amqp-py/xqueue-to-ampq.py
+++ xqueue-to-amqp-py/xqueue-to-ampq.py
@@ -43,6 +43,10 @@ async def xqueue_get(http_client, url, params):
 
 
 async def xqueue_login(http_client, config):
+    """
+    login into EDX xqueue, session cookies are set inside the http_client
+    cookie_jar
+    """
     url = config['xqueue']['base_url'] + '/xqueue/login/'
     data = dict(username=config['xqueue']['username'],
                 password=config['xqueue']['password'])
@@ -52,6 +56,9 @@ async def xqueue_login(http_client, config):
 
 
 async def xqueue_getqueuelen(http_client, config):
+    """
+    returns the number of unprocessed submissions in the xqueue
+    """
     url = config['xqueue']['base_url'] + "/xqueue/get_queuelen/"
     params = dict(queue_name=config['xqueue']['username'])
     body = await xqueue_get(http_client, url, params)
@@ -59,11 +66,28 @@ async def xqueue_getqueuelen(http_client, config):
 
 
 async def xqueue_getsubmission(http_client, config):
+    """
+    consumes and returns one submission from the xqueue.
+    the returned submission follow the forma
+        {
+          xqueue_files: {
+            "submitted_file.zip": "http://<url to download zip>"
+          },
+          xqueue_header: <opaque json object that must be passed when pushing
+          the grade>,
+          xqueue_body: {
+            "student_info": <anonymized student information>,
+            "grader_payload": {"lab": "<name of the lab we must grade>"}
+          }
+        }
+    """
     url = config['xqueue']['base_url'] + '/xqueue/get_submission/'
     params = dict(queue_name=config['xqueue']['username'])
     body = await xqueue_get(http_client, url, params)
     logging.info('Received xqueue submission: {}'.format(body['content']))
     content = json.loads(body['content'])
+    # XQueue response is quite strange; inner json objects are passed as strings
+    # they must be recursively parsed...
     xqueue_body = json.loads(content['xqueue_body'])
     xqueue_body['student_info'] = json.loads(xqueue_body['student_info'])
     xqueue_body['grader_payload'] = json.loads(xqueue_body['grader_payload'])
@@ -73,34 +97,80 @@ async def xqueue_getsubmission(http_client, config):
 
 
 async def xqueue_putresult(http_client, config, xqueue_header, grade):
+    """
+    publishes a grade on EDX xqueue
+        xqueue_header is the opaque object retrieved with the submission
+        grade is a dictionnary with the following format
+          { "correct": False, "score": 7, "msg" : "html message for the student" }
+    """
     url = config['xqueue'][
         'base_url'] + "/xqueue/put_result/?queue_name=" + config['xqueue'][
             'username']
     data = dict(xqueue_header=xqueue_header, xqueue_body=json.dumps(grade))
     body = await xqueue_post(http_client, url, data)
     if body['return_code'] != 0:
-        raise XQueueException('xqueue putresult failed')
+        raise XQueueException('xqueue putresult ({}) failed: {}'.format(
+            xqueue_header, body['content']))
 
 
-async def on_grader_message(message: aiormq.types.DeliveredMessage, config):
-    logging.info("received amqp message:{}\n{}\n".format(
-        message.delivery.routing_key, message.body))
+def signal_to_explanation(signal):
+    """
+    converts a grader signal to a human readable explanation
+    """
+    explanations = {
+        4: 'illegal instruction',
+        6: 'abort, possibly because of a failed assertion',
+        8: 'arithmetic exception',
+        9:
+        'program killed, possibly because of an infinite loop or memory exhaustion',
+        10: 'bus error',
+        11: 'segmentation fault'
+    }
+    if signal in explanations:
+        return explanations[signal]
+    else:
+        return "crash"
 
-    body = json.loads(message.body)
-    result = yaml.load(body['yaml_result'], Loader=yaml.SafeLoader)
-    grade = dict(correct=(result["grade"] == result["max-grade"]),
-                 score=result["grade"],
-                 msg=result["explanation"])
-    xqueue_header = body['opaque']
 
-    async with aiohttp.ClientSession() as session:
-        await xqueue_login(session, config)
-        await xqueue_putresult(session, config, xqueue_header, grade)
+def parse_yaml_result(yaml_result):
+    """
+    converts the grader yaml output to a grading payload
+      { "correct": False, "score": 7, "msg" : "html message for the student" }
+    """
+    result = yaml.load(yaml_result, Loader=yaml.SafeLoader)
+    correct = (result['grade'] == result['max-grade'])
+    score = result['grade']
 
-    await message.channel.basic_ack(message.delivery.delivery_tag)
+    if correct:
+        msg = "<h1>Congratulations, all tests passed!</h1>"
+    else:
+        if 'explanation' in result:
+            msg = "<h1>There has been an error with your submission: </h1>"
+            msg += result['explanation']
+        else:
+            msg = "<h1>Some tests are failing in your submission: </h1>"
+            for group in result['groups']:
+                msg += "<h2>{} (grade {} / {})</h2>".format(
+                    group['description'], group['grade'], group['max-grade'])
+                msg += "<ul>"
+                for test in group['tests']:
+                    if not test['success']:
+                        if 'signal' in test:
+                            explanation = ': ' + signal_to_explanation(
+                                test['signal'])
+                        else:
+                            explanation = ''
+                        msg += "<li> <b>{}</b> (coefficient {}) failed {} </li>".format(
+                            test['description'], test['coefficient'],
+                            explanation)
+                msg += "</ul>"
+            msg += "<br/>"
+
+    return dict(correct=correct, score=score, msg=msg)
 
 
-async def process_submission(config, amqp_channel, submission):
+async def grade_submission(config, amqp_channel, submission):
+    """ sends a submission to amqp-to-test so it is graded """
     zip_name = config['xqueue']['zip_name']
     payload = dict(job_name="xqueue:{}".format(
         submission['xqueue_header']['submission_id']),
@@ -116,19 +186,44 @@ async def process_submission(config, amqp_channel, submission):
                                      routing_key='lab')
 
 
+async def on_grader_message(message: aiormq.types.DeliveredMessage, config):
+    """ called when we receive the result of a submission graded by amqp-to-test """
+    logging.info("received amqp message:{}\n{}\n".format(
+        message.delivery.routing_key, message.body))
+
+    body = json.loads(message.body)
+    grade = parse_yaml_result(body['yaml_result'])
+    xqueue_header = body['opaque']
+
+    async with aiohttp.ClientSession() as session:
+        await xqueue_login(session, config)
+        try:
+            await xqueue_putresult(session, config, xqueue_header, grade)
+        except XQueueException as e:
+            logging.warning("Failed to send back result to EDX: {}".format(e))
+            logging.warning("Maybe the submission_id and key expired")
+            # If the submission is not sent back in a timely manner, EDX
+            # submission key expires. In that case, the student should resubmit
+            # and we better ack to clear the expired job from amqp queue
+
+    await message.channel.basic_ack(message.delivery.delivery_tag)
+
+
 async def poll_xqueue(config, amqp_channel):
+    """ poll xqueue and process waiting submissions """
     async with aiohttp.ClientSession() as session:
         await xqueue_login(session, config)
         queue_len = await xqueue_getqueuelen(session, config)
         logging.info("Polling xqueue ({} jobs submitted)".format(queue_len))
         if queue_len > 0:
             submission = await xqueue_getsubmission(session, config)
-            await process_submission(config, amqp_channel, submission)
+            await grade_submission(config, amqp_channel, submission)
             queue_len -= 1
         return queue_len
 
 
 async def main(config):
+    """ main event loop """
     amqp_uri = "amqp://{}:{}/".format(config['amqp']['host'],
                                       config['amqp']['port'])
     logging.info("Connecting to amqp server " + amqp_uri)
@@ -152,7 +247,7 @@ async def main(config):
         queue_len = await poll_xqueue(config, amqp_channel)
         # throttle requests as per EDX documentation when queue is not growing
         if queue_len == 0:
-            await asyncio.sleep(2)
+            await asyncio.sleep(config['xqueue']['poll_delay'])
 
 
 if __name__ == "__main__":
@@ -166,5 +261,4 @@ if __name__ == "__main__":
     log_level = os.environ.get('LOGLEVEL', 'INFO').upper()
     logging.basicConfig(level=log_level)
     logging.info('started logger at {} level'.format(log_level))
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main(config))
+    asyncio.run(main(config))
